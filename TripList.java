6b1a3d8a9694e7b4fc4d3c50c1d744aa98ddb1e7
sarabm <sara.marsdottir@gmail.com>
2018-03-27 17:02:29 +0000

    fleiri klasar

--- TripList.java
+++ TripList.java
@@ -1,2 +1,21 @@
+import java.util.ArrayList;
+
 public class TripList {
+
+    private ArrayList<Trip>;
+
+    //private DBManager;
+
+    //+ filterTrips(filter: Filter): void
+    public static void filterTrips() {
+    }
+
+    //+ showTrip(tripID: long): void
+    public static void showTrip() {
+    }
+
+    // + resetFilters(): void
+    public static void resetFilters():{
+    }
+
 }
