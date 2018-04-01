daa02754d5506f75ec732ca7de5ef9efb3dd4bd9
sarabm <sara.marsdottir@gmail.com>
2018-04-01 15:51:48 +0000

    Beinagrind fyrir Trip og Triplist

--- TripList.java
+++ TripList.java
@@ -1,21 +1,55 @@
 import java.util.ArrayList;
+import java.util.List;
 
 public class TripList {
 
-    private ArrayList<Trip>;
+    /**
+     * initializa arraylist í smið ekki hér
+     */
+    private List<Trip> trips;
 
-    //private DBManager;
 
-    //+ filterTrips(filter: Filter): void
-    public static void filterTrips() {
+    /**
+     * smiður
+     */
+
+    public TripList(List<Trip> trips) {
+        this.trips = new ArrayList<Trip>();
     }
 
-    //+ showTrip(tripID: long): void
-    public static void showTrip() {
+    /**
+     * + filterTrips(filter: Filter): void
+     *   Pæling hér að sleppa að vera með sér Filter klasa og leita í ferðunum
+     *   beint útfrá viðmótinu þar sem að Filter er aðeins notað í þeim tilgangi,
+     *   hvergi annarstaðar.
+     */
+    public  void filterTrips() {
     }
 
-    // + resetFilters(): void
-    public static void resetFilters():{
+    /**
+     * + showTrip(tripID: long): void
+     */
+    public  void showTrip() {
     }
 
+    /**
+     * + resetFilters(): void
+     * sækja allar ferðir úr DB og setja í arrayList<Trip>
+     * Láta þessa aðferð kalla á getTrips();?
+     */
+    public  void resetFilters() {
+
+    }
+
+
+    /**
+     * + getTrips(): ArrayList<Trip>
+     *  Þurfum við sér aðferð? er ekki hægt að ná í þetta í gegnum DBmanager?
+     * @return skilar lista yfir allar ferðir (út frá filter?)
+     * Kíkja betur á List - Arraylist dæmið
+     */
+    public ArrayList<Trip> getTrips() {
+        //sækja hér úr DB
+        return (ArrayList<Trip>) trips;
+    }
 }
