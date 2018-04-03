8ea78bbf0cacfde8d8e25697936998b91d2114be
bjarkirunars <32462349+bjarkirunars@users.noreply.github.com>
2018-04-03 13:02:57 +0000

    UserProfile uppfært

--- UserProfile.java
+++ UserProfile.java
@@ -3,4 +3,53 @@ public class UserProfile {
     /**
      * Getum við notað eitthvað sem er til í JAVA?
      */
+
+    private String userName;
+    private boolean tourManager;
+    private boolean tourBooker;
+
+
+    /**
+     * Smiður
+     * @param userName
+     * @param tourManager
+     * @param tourBooker
+     */
+    public UserProfile(String userName, boolean tourManager, boolean tourBooker) {
+        this.userName = userName;
+        this.tourManager = tourManager;
+        this.tourBooker = tourBooker;
+    }
+
+    public UserProfile() {
+    }
+
+    public boolean deleteAccount() {
+        return true;
+    }
+
+    public void editProfile() {
+
+    }
+
+    public boolean changePassword(String newPassword) {
+        return true;
+    }
+
+    public TripList listTrips() {
+        return new TripList( List<Booking.Trip> trips);
+    }
+
+    public Booking listBookings() {
+        return new Booking(bookingNo, personInfo, tripId, noGuest);
+
+    }
+
+    public Review listReviews() {
+        return new Review(date, rewiewID, user, reviewText, tripID, visability);
+    }
+
+
+
+
 }
